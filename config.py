99a9589f3ff5048a76f6bd725a9bbb76e294aab2
Tobias Chielens <tobias.chielens@gmail.com>
2020-06-30 16:48:35 +0200

    cleanup

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 config = {
-    'threebot-privatekey': 'KBEs2Z7O7o/MzGdYTNR2FT0jjrRLAtFuWtZcnU8inWo=',
     'threebot-appid': 'localhost:5000',
     'onlyofficeUrl': 'http://localhost',
     'loginUrl': 'https://login.staging.jimber.org',
